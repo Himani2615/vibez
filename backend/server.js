4a3742bfcfb5fee5b6958487fb721acb7d75dd2c
Himani2615 <himanisingh26102005@gmail.com>
2024-08-13 03:36:15 +0530

    Backend complete

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import cookieParser from "cookie-parser";
+import express from "express";
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+import connectToDb from "./db/connectToMongoDB.js";
 
 const app =express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res) => {
-  res.send("Hello ");
-})
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(PORT,() => console.log(`Server is listening on port ${PORT}`));
+// app.get("/",(req,res) => {
+//   res.send("Hello ");
+// })
+
+app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes)
+app.use('/api/users',userRoutes)
+
+app.listen(PORT,() => {
+  connectToDb();
+  console.log(`Server is listening on port ${PORT}`)
+  });
