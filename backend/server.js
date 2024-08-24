677eede2d28460919be6f88ab3b7446c53a891ca
Himani2615 <himanisingh26102005@gmail.com>
2024-08-24 22:25:12 +0530

    Added Socket io

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToDb from "./db/connectToMongoDB.js";
+import { app,server } from "./socket/socket.js";
 
-const app =express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -22,7 +22,7 @@ app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
   connectToDb();
   console.log(`Server is listening on port ${PORT}`)
   });
