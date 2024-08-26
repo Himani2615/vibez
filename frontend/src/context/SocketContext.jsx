70c6e8897f3b6efe1fb0062adc24808829f36782
Himani2615 <himanisingh26102005@gmail.com>
2024-08-27 02:17:36 +0530

    Fix socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000",{
+			const socket = io("https://vibez-b3pl.onrender.com/",{
         query:{
           userId: authUser._id
     }});
